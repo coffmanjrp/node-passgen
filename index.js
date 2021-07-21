07c7f5f6505a9d5b99e20cb01b9e056e04964c39
coffmanjrp <coffmanjrp@gmail.com>
2021-07-20 18:25:46 -0700

    Creating commands and options

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log(process.argv);
+const program = require('commander');
+
+program.version('1.0.0').description('Simple Password Generator');
+
+program
+  .option('-l, --length <number>', 'length of password', '8')
+  .option('-s, --save', 'save password to password.txt')
+  .option('-nn, --no-numbers', 'remove numbers')
+  .option('-ns, --no-symbols', 'remove symbols')
+  .parse();
+
+const { length, save, numbers, symbols } = program.opts();
+
+console.log(numbers, symbols);
