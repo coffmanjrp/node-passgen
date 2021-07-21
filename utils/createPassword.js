c40f9402e902ee29a6050c63be95208dbaea9ea7
coffmanjrp <coffmanjrp@gmail.com>
2021-07-20 18:46:09 -0700

    Copy to clipboard

--- utils/createPassword.js
+++ utils/createPassword.js
@@ -10,7 +10,7 @@ const createPassword = (length = 8, hasNumbers = true, hasSymbols = true) => {
 };
 
 const generatePassword = (length, chars) => {
-  let password = 'abc';
+  let password = '';
   for (let i = 0; i < length; i++) {
     password += chars.charAt(Math.floor(Math.random() * chars.length));
   }
